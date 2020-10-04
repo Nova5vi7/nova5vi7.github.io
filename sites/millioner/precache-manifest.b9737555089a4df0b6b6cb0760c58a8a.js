28e99b59aeb330e00d2f28ed0f09cf6f5f355844
Forest_Hub <anatolijk096@gmail.com>
2020-10-04 20:34:08 +0300

    add millioner test

---
+++ sites/millioner/precache-manifest.b9737555089a4df0b6b6cb0760c58a8a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e58e8cb67886973d884873ebf9390c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d8d2f25f1a0d7e4eefe",
+    "url": "/static/css/main.39d79025.chunk.css"
+  },
+  {
+    "revision": "f6e56e1b2dea1400c049",
+    "url": "/static/js/2.c05a76bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c05a76bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d8d2f25f1a0d7e4eefe",
+    "url": "/static/js/main.c9254feb.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "07bf7015e8a8293840ca696e7b88275d",
+    "url": "/static/media/Inter-Regular.07bf7015.eot"
+  },
+  {
+    "revision": "678f9520fa54a52fb87f57e7efbabdef",
+    "url": "/static/media/Inter-Regular.678f9520.woff"
+  },
+  {
+    "revision": "bed1dd5f2dc75993805fbf445e23a6c3",
+    "url": "/static/media/Inter-Regular.bed1dd5f.ttf"
+  },
+  {
+    "revision": "ccab500255401f315afbac2b00898f87",
+    "url": "/static/media/Inter-Regular.ccab5002.svg"
+  },
+  {
+    "revision": "4260b090187d3f7a73424a7a008414e3",
+    "url": "/static/media/Inter-SemiBold.4260b090.eot"
+  },
+  {
+    "revision": "aac0d6030b83c71e7ebb561097c28d26",
+    "url": "/static/media/Inter-SemiBold.aac0d603.svg"
+  },
+  {
+    "revision": "bb1398cc02de18ecb32d4a68f693f43e",
+    "url": "/static/media/Inter-SemiBold.bb1398cc.ttf"
+  },
+  {
+    "revision": "d5d9d7641d430bf4e32a89ada03275d9",
+    "url": "/static/media/Inter-SemiBold.d5d9d764.woff"
+  },
+  {
+    "revision": "f8c75481696354bfdabed0ba328d3476",
+    "url": "/static/media/hand.f8c75481.png"
+  },
+  {
+    "revision": "c7cf242a543ae2b7b7cfd4cfe7087f14",
+    "url": "/static/media/main-bg.c7cf242a.png"
+  }
+]);
